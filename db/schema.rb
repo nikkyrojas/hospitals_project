331bd24fda208881bdb93237029e113c4ad4fddc
Nikky Rojas <rojas7313@gmail.com>
2022-07-08 17:29:51 -0600

    add view of indvidual hospital

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_225547) do
+ActiveRecord::Schema.define(version: 2022_07_08_225740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_07_08_225547) do
     t.boolean "probono"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "hospital_id"
+    t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
   end
 
   create_table "hospitals", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_07_08_225547) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doctors", "hospitals"
 end
